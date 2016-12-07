025b76d6cefaa2043a811668358d52be2ee49c45
Skurt <betina1995@hotmail.com>
2016-12-07 15:03:42 +0100

    Update VIEWS_SQL_file.sql

--- VIEWS_SQL_file.sql
+++ VIEWS_SQL_file.sql
@@ -19,3 +19,55 @@ SELECT *
 FROM Finished_Courses NATURAL JOIN Courses WHERE Grade IN ( '3', '4', '5');
 
 
+
+
+
+****************************************************************************
+
+-- TDA357_050 - Betina Andersson, Shahad Naji & Fressia Merino
+--_____________________________________________________
+
+
+CREATE VIEW StudentsFollowing AS
+  SELECT stud_id, stud_name, ssn, prog_name, branch_name FROM Students NATURAL full OUTER
+  JOIN Stud_Chooses_Branch;
+  
+--——————————————————————————
+CREATE VIEW FinishedCourses AS 
+SELECT course_name, credits, stud_id, grade
+FROM Courses NATURAL JOIN finished_courses;
+
+
+--—————————————————————————
+
+CREATE VIEW Registrations AS 
+SELECT * 
+FROM Students_Registered_To_Course NATURAL full OUTER JOIN Waiting_List;
+
+--________________________
+CREATE VIEW PassedCourses AS 
+SELECT stud_id, course_code, grade, credits
+FROM Finished_Courses NATURAL JOIN Courses WHERE Grade IN ( '3', '4', '5');
+
+------------------------------------------------------------------------------
+CREATE VIEW UnreadMandatory as
+
+select stud_id, course_code
+from students 
+join mandatory_course_program 
+on students.prog_name = mandatory_course_program.prog_name
+UNION
+select stud_id, course_code 
+from students 
+join mandatory_course_branch 
+on stud_chooses_branch.branch_name = mandatory_course_branch AND stud_choosees_branch.branch_name NOT IN (select stud_id, course_code from PassedCourses);
+
+EXCEPT stud_id, course_code
+from PassedCourses;
+
+-------------------------------------------------------------------------------
+
+
+SELECT stud_id, course_code, prog_name, branch_name
+FROM students NATURAL  FULL OUTER JOIN mandatory_course_program
+join mandatory_course_branch;
